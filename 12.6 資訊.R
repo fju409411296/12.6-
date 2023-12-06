04b32a4f7201b79a0863ef088b3e6cfcd3f32f1f
le402n49\uer <tnkao91@gmail.com>
2023-12-06 16:53:30 +0800

    擷取ptt資訊

--- 12.6 資訊.R
+++ 12.6 資訊.R
@@ -75,3 +75,18 @@ website = read_html(URL)
 
 needed_txt = website %>% html_nodes("tr") %>% html_text()
 needed_txt
+
+
+URL = "https://www.ptt.cc/bbs/AllTogether/index3245.html"
+website = read_html(URL)
+
+needed_html = website %>% html_nodes("a")
+needed_html
+#只留字
+needed_txt = needed_html %>% html_text()
+needed_txt
+#取徵女
+intrested_pos = grep("[徵女]", needed_txt, fixed = TRUE)
+needed_txt[intrested_pos]
+#取得以上連結
+needed_link = needed_html[intrested_pos] %>% html_attr("href")
